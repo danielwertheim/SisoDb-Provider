e3c344f8a3dcade6dce0aed6835b1eccc9355b54
Daniel Wertheim <daniel@wertheim.se>
2011-10-12 20:58:56 +0200

    -Added GuidValue -Using StructureSchema.IdMemberName instead of hard "StructureId"

--- Source/Projects/SisoDb/DbSchema/IndexStorageSchema.cs
+++ Source/Projects/SisoDb/DbSchema/IndexStorageSchema.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using NCore;
+using NCore.Reflections;
 using PineCone.Structures.Schemas;
 using SisoDb.Providers;
+using SisoDb.Resources;
 
 namespace SisoDb.DbSchema
 {
@@ -9,15 +12,16 @@ namespace SisoDb.DbSchema
     {
         public static class Fields
         {
-            public static readonly SchemaField StructureId = new SchemaField(0, "StructureId");
+            public static readonly SchemaField StructureId = new SchemaField(0, StructureSchema.IdMemberName);
             public static readonly SchemaField MemberPath = new SchemaField(1, "MemberPath");
             public static readonly SchemaField StringValue = new SchemaField(2, "StringValue");
             public static readonly SchemaField IntegerValue = new SchemaField(3, "IntegerValue");
             public static readonly SchemaField FractalValue = new SchemaField(4, "FractalValue");
             public static readonly SchemaField DateTimeValue = new SchemaField(5, "DateTimeValue");
             public static readonly SchemaField BoolValue = new SchemaField(6, "BoolValue");
+            public static readonly SchemaField GuidValue = new SchemaField(7, "GuidValue");
         }
-
+        
         public static readonly SchemaField[] OrderedFields = new[]
         {
             Fields.StructureId,
@@ -26,8 +30,32 @@ namespace SisoDb.DbSchema
             Fields.IntegerValue,
             Fields.FractalValue,
             Fields.DateTimeValue,
-            Fields.BoolValue
+            Fields.BoolValue,
+            Fields.GuidValue
         };
+
+        public static SchemaField GetValueSchemaFieldForType(Type dataType)
+        {
+            if (dataType.IsStringType())
+                return Fields.StringValue;
+
+            if (dataType.IsIntegerNumberType())
+                return Fields.IntegerValue;
+
+            if (dataType.IsFractalNumberType())
+                return Fields.FractalValue;
+
+            if (dataType.IsAnyDateTimeType())
+                return Fields.DateTimeValue;
+
+            if (dataType.IsAnyBoolType())
+                return Fields.BoolValue;
+
+            if (dataType.IsAnyGuidType())
+                return Fields.GuidValue;
+
+            throw new SisoDbException(ExceptionMessages.IndexStorageSchema_MissingValueSchemaField.Inject(dataType.Name));
+        }
 
         public IndexStorageSchema(IStructureSchema structureSchema)
             : base(structureSchema, structureSchema.GetIndexesTableName())
