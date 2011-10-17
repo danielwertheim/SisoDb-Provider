51d2450a8b8023b1d7f217e01b0ab12fa10a74cf
Daniel Wertheim <daniel@wertheim.se>
2011-10-18 00:08:44 +0200

    -Converted last UpdateStructureSet tests from old integrationtests to specifications.

--- Source/Tests/SisoDb.Specifications/Sql2008/Database/UpdateStructureSet.cs
+++ Source/Tests/SisoDb.Specifications/Sql2008/Database/UpdateStructureSet.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Machine.Specifications;
+using NCore;
+using SisoDb.Resources;
 using SisoDb.Sql2008;
 using SisoDb.Testing;
 
@@ -98,20 +102,13 @@ namespace SisoDb.Specifications.Sql2008.Database
                     return StructureSetUpdaterStatuses.Keep;
                 });
 
-            It should_have_kept_and_updated_the_first_and_last_items = () =>
+            It should_have_kept_and_updated_all_members_except_id_of_the_first_item = () =>
             {
                 using (var q = TestContext.Database.CreateQueryEngine())
                 {
                     var newItem1 = q.GetById<ModelNew.ItemForPropChange>(_orgItem1Id);
                     newItem1.ShouldNotBeNull();
                     newItem1.NewString1 = "A";
-
-                    var newItem2 = q.GetById<ModelNew.ItemForPropChange>(_orgItem2Id);
-                    newItem2.ShouldBeNull();
-
-                    var newItem3 = q.GetById<ModelNew.ItemForPropChange>(_orgItem3Id);
-                    newItem3.ShouldNotBeNull();
-                    newItem3.NewString1 = "C";
                 }
             };
 
@@ -121,6 +118,16 @@ namespace SisoDb.Specifications.Sql2008.Database
                 {
                     var newItem2 = q.GetById<ModelNew.ItemForPropChange>(_orgItem2Id);
                     newItem2.ShouldBeNull();
+                }
+            };
+
+            It should_have_kept_and_updated_all_members_except_id_of_the_last_item = () =>
+            {
+                using (var q = TestContext.Database.CreateQueryEngine())
+                {
+                    var newItem3 = q.GetById<ModelNew.ItemForPropChange>(_orgItem3Id);
+                    newItem3.ShouldNotBeNull();
+                    newItem3.NewString1 = "C";
                 }
             };
 
@@ -150,7 +157,7 @@ namespace SisoDb.Specifications.Sql2008.Database
                 TestContext = TestContextFactory.Create(StorageProviders.Sql2008);
             };
 
-            private Because of = () =>
+            Because of = () =>
                 TestContext.Database.UpdateStructureSet<ModelOld.GuidItemForPropChange, ModelNew.GuidItemForPropChange>(
                 (oldItem, newItem) =>
                 {
@@ -162,20 +169,13 @@ namespace SisoDb.Specifications.Sql2008.Database
                     return StructureSetUpdaterStatuses.Keep;
                 });
 
-            It should_have_kept_and_updated_the_first_and_last_items = () =>
+            It should_have_kept_and_updated_all_members_except_id_of_the_first_item = () =>
             {
                 using (var q = TestContext.Database.CreateQueryEngine())
                 {
                     var newItem1 = q.GetById<ModelNew.GuidItemForPropChange>(_orgItem1Id);
                     newItem1.ShouldNotBeNull();
                     newItem1.NewString1 = "A";
-
-                    var newItem2 = q.GetById<ModelNew.GuidItemForPropChange>(_orgItem2Id);
-                    newItem2.ShouldBeNull();
-
-                    var newItem3 = q.GetById<ModelNew.GuidItemForPropChange>(_orgItem3Id);
-                    newItem3.ShouldNotBeNull();
-                    newItem3.NewString1 = "C";
                 }
             };
 
@@ -188,7 +188,325 @@ namespace SisoDb.Specifications.Sql2008.Database
                 }
             };
 
+            It should_have_kept_and_updated_all_members_except_id_of_the_last_item = () =>
+            {
+                using (var q = TestContext.Database.CreateQueryEngine())
+                {
+                    var newItem3 = q.GetById<ModelNew.GuidItemForPropChange>(_orgItem3Id);
+                    newItem3.ShouldNotBeNull();
+                    newItem3.NewString1 = "C";
+                }
+            };
+
             private static Guid _orgItem1Id, _orgItem2Id, _orgItem3Id;
+        }
+
+        [Subject(typeof(Sql2008Database), "Update structure set")]
+        public class when_two_structures_exists_and_abort_is_made_on_last : SpecificationBase
+        {
+            Establish context = () =>
+            {
+                var orgItem1 = new ModelOld.GuidItemForPropChange { Int1 = 142, String1 = "A" };
+                var orgItem2 = new ModelOld.GuidItemForPropChange { Int1 = 242, String1 = "B" };
+
+                var testContext = TestContextFactory.Create(StorageProviders.Sql2008);
+                using (var uow = testContext.Database.CreateUnitOfWork())
+                {
+                    uow.InsertMany(new[] { orgItem1, orgItem2 });
+                    uow.Commit();
+                }
+
+                _orgItem1Id = orgItem1.StructureId;
+                _orgItem2Id = orgItem2.StructureId;
+
+                TestContext = TestContextFactory.Create(StorageProviders.Sql2008);
+            };
+
+            Because of = () =>
+                TestContext.Database.UpdateStructureSet<ModelOld.GuidItemForPropChange, ModelNew.GuidItemForPropChange>(
+                (oldItem, newItem) =>
+                {
+                    newItem.NewString1 = "New" + oldItem.String1;
+
+                    if (oldItem.StructureId == _orgItem2Id)
+                        return StructureSetUpdaterStatuses.Abort;
+
+                    return StructureSetUpdaterStatuses.Keep;
+                });
+
+            It should_have_kept_old_items_untouched = () =>
+            {
+                TestContext.Database.StructureSchemas.Clear();
+
+                List<ModelOld.GuidItemForPropChange> items;
+
+                using (var q = TestContext.Database.CreateQueryEngine())
+                {
+                    items = q.GetAll<ModelOld.GuidItemForPropChange>().ToList();
+                }
+
+                items[0].StructureId.ShouldEqual(_orgItem1Id);
+                items[0].Int1.ShouldEqual(142);
+                items[0].String1.ShouldEqual("A");
+
+                items[1].StructureId.ShouldEqual(_orgItem2Id);
+                items[1].Int1.ShouldEqual(242);
+                items[1].String1.ShouldEqual("B");
+            };
+
+            private static Guid _orgItem1Id, _orgItem2Id;
+        }
+
+        [Subject(typeof(Sql2008Database), "Update structure set")]
+        public class when_the_second_of_two_new_structures_does_not_get_an_identity_id : SpecificationBase
+        {
+            Establish context = () =>
+            {
+                var orgItem1 = new ModelOld.ItemForPropChange { Int1 = 142, String1 = "A" };
+                var orgItem2 = new ModelOld.ItemForPropChange { Int1 = 242, String1 = "B" };
+
+                var testContext = TestContextFactory.Create(StorageProviders.Sql2008);
+                using (var uow = testContext.Database.CreateUnitOfWork())
+                {
+                    uow.InsertMany(new[] { orgItem1, orgItem2 });
+                    uow.Commit();
+                }
+
+                _orgItem1Id = orgItem1.StructureId;
+                _orgItem2Id = orgItem2.StructureId;
+
+                TestContext = TestContextFactory.Create(StorageProviders.Sql2008);
+            };
+
+            Because of = () => _exception = Catch.Exception(() =>
+                TestContext.Database.UpdateStructureSet<ModelOld.ItemForPropChange, ModelNew.ItemForPropChange>(
+                (oldItem, newItem) =>
+                {
+                    if (oldItem.StructureId == _orgItem2Id)
+                        newItem.StructureId = 0;
+
+                    newItem.NewString1 = "New" + oldItem.String1;
+
+                    return StructureSetUpdaterStatuses.Keep;
+                }));
+
+            It should_have_failed = () => _exception.ShouldNotBeNull();
+
+            It should_have_descriptive_message = () =>
+                _exception.Message.ShouldEqual(ExceptionMessages.SqlStructureSetUpdater_NewIdDoesNotMatchOldId.Inject(0, _orgItem2Id));
+
+            It should_have_kept_old_items_untouched = () =>
+            {
+                TestContext.Database.StructureSchemas.Clear();
+
+                List<ModelOld.ItemForPropChange> items;
+
+                using (var q = TestContext.Database.CreateQueryEngine())
+                {
+                    items = q.GetAll<ModelOld.ItemForPropChange>().ToList();
+                }
+
+                items[0].StructureId.ShouldEqual(_orgItem1Id);
+                items[0].Int1.ShouldEqual(142);
+                items[0].String1.ShouldEqual("A");
+
+                items[1].StructureId.ShouldEqual(_orgItem2Id);
+                items[1].Int1.ShouldEqual(242);
+                items[1].String1.ShouldEqual("B");
+            };
+
+            private static int _orgItem1Id, _orgItem2Id;
+            private static Exception _exception;
+        }
+
+        [Subject(typeof(Sql2008Database), "Update structure set")]
+        public class when_the_second_of_two_new_structures_does_not_get_an_guid_id : SpecificationBase
+        {
+            Establish context = () =>
+            {
+                var orgItem1 = new ModelOld.GuidItemForPropChange { Int1 = 142, String1 = "A" };
+                var orgItem2 = new ModelOld.GuidItemForPropChange { Int1 = 242, String1 = "B" };
+
+                var testContext = TestContextFactory.Create(StorageProviders.Sql2008);
+                using (var uow = testContext.Database.CreateUnitOfWork())
+                {
+                    uow.InsertMany(new[] { orgItem1, orgItem2 });
+                    uow.Commit();
+                }
+
+                _orgItem1Id = orgItem1.StructureId;
+                _orgItem2Id = orgItem2.StructureId;
+
+                TestContext = TestContextFactory.Create(StorageProviders.Sql2008);
+            };
+
+            Because of = () => _exception = Catch.Exception(() =>
+                TestContext.Database.UpdateStructureSet<ModelOld.GuidItemForPropChange, ModelNew.GuidItemForPropChange>(
+                (oldItem, newItem) =>
+                {
+                    if(oldItem.StructureId == _orgItem2Id)
+                        newItem.StructureId = Guid.Empty;
+                    
+                    newItem.NewString1 = "New" + oldItem.String1;
+
+                    return StructureSetUpdaterStatuses.Keep;
+                }));
+
+            It should_have_failed = () => _exception.ShouldNotBeNull();
+
+            It should_have_descriptive_message = () =>
+                _exception.Message.ShouldEqual(ExceptionMessages.SqlStructureSetUpdater_NewIdDoesNotMatchOldId.Inject(Guid.Empty, _orgItem2Id));
+
+            It should_have_kept_old_items_untouched = () =>
+            {
+                TestContext.Database.StructureSchemas.Clear();
+
+                List<ModelOld.GuidItemForPropChange> items;
+
+                using (var q = TestContext.Database.CreateQueryEngine())
+                {
+                    items = q.GetAll<ModelOld.GuidItemForPropChange>().ToList();
+                }
+
+                items[0].StructureId.ShouldEqual(_orgItem1Id);
+                items[0].Int1.ShouldEqual(142);
+                items[0].String1.ShouldEqual("A");
+
+                items[1].StructureId.ShouldEqual(_orgItem2Id);
+                items[1].Int1.ShouldEqual(242);
+                items[1].String1.ShouldEqual("B");
+            };
+
+            private static Guid _orgItem1Id, _orgItem2Id;
+            private static Exception _exception;
+        }
+
+        [Subject(typeof(Sql2008Database), "Update structure set")]
+        public class when_the_second_of_two_new_structures_does_get_an_new_identity_id : SpecificationBase
+        {
+            Establish context = () =>
+            {
+                var orgItem1 = new ModelOld.ItemForPropChange { Int1 = 142, String1 = "A" };
+                var orgItem2 = new ModelOld.ItemForPropChange { Int1 = 242, String1 = "B" };
+
+                var testContext = TestContextFactory.Create(StorageProviders.Sql2008);
+                using (var uow = testContext.Database.CreateUnitOfWork())
+                {
+                    uow.InsertMany(new[] { orgItem1, orgItem2 });
+                    uow.Commit();
+                }
+
+                _orgItem1Id = orgItem1.StructureId;
+                _orgItem2Id = orgItem2.StructureId;
+                _newItem2Id = _orgItem2Id + 1;
+
+                TestContext = TestContextFactory.Create(StorageProviders.Sql2008);
+            };
+
+            Because of = () => _exception = Catch.Exception(() =>
+                TestContext.Database.UpdateStructureSet<ModelOld.ItemForPropChange, ModelNew.ItemForPropChange>(
+                (oldItem, newItem) =>
+                {
+                    if (oldItem.StructureId == _orgItem2Id)
+                        newItem.StructureId = _newItem2Id;
+
+                    newItem.NewString1 = "New" + oldItem.String1;
+
+                    return StructureSetUpdaterStatuses.Keep;
+                }));
+
+            It should_have_failed = () => _exception.ShouldNotBeNull();
+
+            It should_have_descriptive_message = () =>
+                _exception.Message.ShouldEqual(ExceptionMessages.SqlStructureSetUpdater_NewIdDoesNotMatchOldId.Inject(_newItem2Id, _orgItem2Id));
+
+            It should_have_kept_old_items_untouched = () =>
+            {
+                TestContext.Database.StructureSchemas.Clear();
+
+                List<ModelOld.ItemForPropChange> items;
+
+                using (var q = TestContext.Database.CreateQueryEngine())
+                {
+                    items = q.GetAll<ModelOld.ItemForPropChange>().ToList();
+                }
+
+                items[0].StructureId.ShouldEqual(_orgItem1Id);
+                items[0].Int1.ShouldEqual(142);
+                items[0].String1.ShouldEqual("A");
+
+                items[1].StructureId.ShouldEqual(_orgItem2Id);
+                items[1].Int1.ShouldEqual(242);
+                items[1].String1.ShouldEqual("B");
+            };
+
+            private static int _orgItem1Id, _orgItem2Id;
+            private static int _newItem2Id;
+            private static Exception _exception;
+        }
+
+        [Subject(typeof(Sql2008Database), "Update structure set")]
+        public class when_the_second_of_two_new_structures_does_get_an_new_guid_id : SpecificationBase
+        {
+            Establish context = () =>
+            {
+                var orgItem1 = new ModelOld.GuidItemForPropChange { Int1 = 142, String1 = "A" };
+                var orgItem2 = new ModelOld.GuidItemForPropChange { Int1 = 242, String1 = "B" };
+
+                var testContext = TestContextFactory.Create(StorageProviders.Sql2008);
+                using (var uow = testContext.Database.CreateUnitOfWork())
+                {
+                    uow.InsertMany(new[] { orgItem1, orgItem2 });
+                    uow.Commit();
+                }
+
+                _orgItem1Id = orgItem1.StructureId;
+                _orgItem2Id = orgItem2.StructureId;
+                _newItem2Id = Guid.Parse("ED563B06-7D30-4136-BE05-B12A0D5B9798");
+
+                TestContext = TestContextFactory.Create(StorageProviders.Sql2008);
+            };
+
+            Because of = () => _exception = Catch.Exception(() =>
+                TestContext.Database.UpdateStructureSet<ModelOld.GuidItemForPropChange, ModelNew.GuidItemForPropChange>(
+                (oldItem, newItem) =>
+                {
+                    if (oldItem.StructureId == _orgItem2Id)
+                        newItem.StructureId = _newItem2Id;
+
+                    newItem.NewString1 = "New" + oldItem.String1;
+
+                    return StructureSetUpdaterStatuses.Keep;
+                }));
+
+            It should_have_failed = () => _exception.ShouldNotBeNull();
+
+            It should_have_descriptive_message = () =>
+                _exception.Message.ShouldEqual(ExceptionMessages.SqlStructureSetUpdater_NewIdDoesNotMatchOldId.Inject(_newItem2Id, _orgItem2Id));
+
+            It should_have_kept_old_items_untouched = () =>
+            {
+                TestContext.Database.StructureSchemas.Clear();
+
+                List<ModelOld.GuidItemForPropChange> items;
+
+                using (var q = TestContext.Database.CreateQueryEngine())
+                {
+                    items = q.GetAll<ModelOld.GuidItemForPropChange>().ToList();
+                }
+
+                items[0].StructureId.ShouldEqual(_orgItem1Id);
+                items[0].Int1.ShouldEqual(142);
+                items[0].String1.ShouldEqual("A");
+
+                items[1].StructureId.ShouldEqual(_orgItem2Id);
+                items[1].Int1.ShouldEqual(242);
+                items[1].String1.ShouldEqual("B");
+            };
+
+            private static Guid _orgItem1Id, _orgItem2Id;
+            private static Guid _newItem2Id;
+            private static Exception _exception;
         }
 
         namespace ModelComplexUpdates
