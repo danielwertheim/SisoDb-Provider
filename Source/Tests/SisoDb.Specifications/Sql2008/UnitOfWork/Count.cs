e3e74d8403b31a3446223e41c3072b2d163d7f07
Daniel Wertheim <daniel@wertheim.se>
2011-10-28 20:10:15 +0200

    Ported count tests to specs.

--- Source/Tests/SisoDb.Specifications/Sql2008/UnitOfWork/Count.cs
+++ Source/Tests/SisoDb.Specifications/Sql2008/UnitOfWork/Count.cs
@@ -5,9 +5,9 @@ using SisoDb.Testing;
 
 namespace SisoDb.Specifications.Sql2008.UnitOfWork
 {
-    namespace Queries
+    namespace Count
     {
-        [Subject(typeof(Sql2008UnitOfWork))]
+        [Subject(typeof(Sql2008UnitOfWork), "Count")]
         public class when_counting_on_item_that_does_not_exist : SpecificationBase
         {
             Establish context = () => TestContext = TestContextFactory.Create(StorageProviders.Sql2008);
@@ -23,7 +23,7 @@ namespace SisoDb.Specifications.Sql2008.UnitOfWork
             private static int _count;
         }
 
-        [Subject(typeof(Sql2008UnitOfWork))]
+        [Subject(typeof(Sql2008UnitOfWork), "Count")]
         public class when_counting_using_expression_on_item_that_does_not_exist : SpecificationBase
         {
             Establish context = () => TestContext = TestContextFactory.Create(StorageProviders.Sql2008);
@@ -39,8 +39,58 @@ namespace SisoDb.Specifications.Sql2008.UnitOfWork
             private static int _count;
         }
 
-        [Subject(typeof(Sql2008UnitOfWork))]
-        public class when_on_two_of_three_items_that_is_uncommitted_mode : SpecificationBase
+        [Subject(typeof(Sql2008UnitOfWork), "Count")]
+        public class when_counting_on_three_items : SpecificationBase
+        {
+            Establish context = () =>
+            {
+                TestContext = TestContextFactory.Create(StorageProviders.Sql2008);
+                TestContext.Database.UoW().InsertMany(new[]
+                {
+                    new QueryGuidItem { IntValue = 1 },
+                    new QueryGuidItem { IntValue = 2 },
+                    new QueryGuidItem { IntValue = 3 }
+                });
+            };
+
+            Because of = () =>
+            {
+                using (var uow = TestContext.Database.CreateUnitOfWork())
+                    _count = uow.Count<QueryGuidItem>();
+            };
+
+            It should_result_in_count_of_3 = () => _count.ShouldEqual(3);
+
+            private static int _count;
+        }
+        
+        [Subject(typeof(Sql2008UnitOfWork), "Count")]
+        public class when_counting_using_expression_matching_all_three_items : SpecificationBase
+        {
+            Establish context = () =>
+            {
+                TestContext = TestContextFactory.Create(StorageProviders.Sql2008);
+                TestContext.Database.UoW().InsertMany(new[]
+                {
+                    new QueryGuidItem { IntValue = 1 },
+                    new QueryGuidItem { IntValue = 2 },
+                    new QueryGuidItem { IntValue = 3 }
+                });
+            };
+
+            Because of = () =>
+            {
+                using (var uow = TestContext.Database.CreateUnitOfWork())
+                    _count = uow.Count<QueryGuidItem>(x => x.IntValue > 0);
+            };
+
+            It should_result_in_count_of_3 = () => _count.ShouldEqual(3);
+
+            private static int _count;
+        }
+
+        [Subject(typeof(Sql2008UnitOfWork), "Count")]
+        public class when_counting_using_expression_matching_two_of_three_items_that_is_in_uncommitted_mode : SpecificationBase
         {
             Establish context = () => TestContext = TestContextFactory.Create(StorageProviders.Sql2008);
 
