338383c12a98f91fbea148b69c86f16924bd7eed
Daniel Wertheim <daniel@wertheim.se>
2011-11-14 15:29:05 +0100

    - Fixed, GetByIdInterval, only supported if identities. - Moved NamedQueryAs to Advanced queries. - Updated release notes.

--- Source/Projects/SisoDb/IQueryEngine.cs
+++ Source/Projects/SisoDb/IQueryEngine.cs
@@ -11,6 +11,11 @@ namespace SisoDb
     public interface IQueryEngine : IDisposable
     {
         /// <summary>
+        /// Advances querying members.
+        /// </summary>
+        IAdvancedQueryEngine Advanced { get; }
+
+        /// <summary>
         /// Issues a simple count for how many structures there are
         /// in the specified structure type <typeparamref name="T"/>.
         /// </summary>
@@ -190,44 +195,6 @@ namespace SisoDb
         /// <returns>Json representation of all structures (<typeparamref name="T"/>)
         /// or empty IEnumerable of <see cref="string"/>.</returns>
         IEnumerable<string> GetAllAsJson<T>(Action<IGetCommandBuilder<T>> commandInitializer)
-            where T : class;
-
-        /// <summary>
-        /// Lets you invoke stored procedures that returns Json,
-        /// which will get deserialized to <typeparamref name="T"/>.
-        /// </summary>
-        /// <typeparam name="T">
-        /// Structure type, used as a contract defining the scheme.</typeparam>
-        /// <param name="query"></param>
-        /// <returns>Empty or populated IEnumerable of <typeparamref name="T"/>.</returns>
-        IEnumerable<T> NamedQuery<T>(INamedQuery query)
-            where T : class;
-
-        /// <summary>
-        /// Lets you invoke stored procedures that returns Json,
-        /// which will get deserialized to <typeparamref name="TOut"/>.
-        /// </summary>
-        /// <typeparam name="TContract">
-        /// Structure type, used as a contract defining the scheme.</typeparam>
-        /// <typeparam name="TOut">
-        /// Determines the type you want your structure deserialized to and returned as.</typeparam>
-        /// <param name="query"></param>
-        /// <returns>Empty or populated IEnumerable of <typeparamref name="TOut"/>.</returns>
-        IEnumerable<TOut> NamedQueryAs<TContract, TOut>(INamedQuery query)
-            where TContract : class
-            where TOut : class;
-
-        /// <summary>
-        /// Lets you invoke stored procedures that returns Json.
-        /// This is the most effective return type, since the Json is stored in the database,
-        /// no deserialization will take place. 
-        /// </summary>
-        /// <typeparam name="T">
-        /// Structure type, used as a contract defining the scheme.</typeparam>
-        /// <param name="query"></param>
-        /// <returns>Json representation of structures (<typeparamref name="T"/>)
-        /// or empty IEnumerable of <see cref="string"/>.</returns>
-        IEnumerable<string> NamedQueryAsJson<T>(INamedQuery query)
             where T : class;
 
         /// <summary>
