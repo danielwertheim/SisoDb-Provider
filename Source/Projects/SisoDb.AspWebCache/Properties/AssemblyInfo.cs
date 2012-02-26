30abf1aa344aa34867c645d219cf6ea9eb1ade1d
Daniel Wertheim <daniel@wertheim.se>
2012-02-26 20:10:00 +0100

    WebCache is now AspWebCache which is implemented over the Asp WebCache

--- Source/Projects/SisoDb.AspWebCache/Properties/AssemblyInfo.cs
+++ Source/Projects/SisoDb.AspWebCache/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SisoDb.WebCache")]
+[assembly: AssemblyTitle("SisoDb.AspWebCache")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
