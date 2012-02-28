ed6179fced8bd2f16a2f6224c1cd34ede3135e66
Daniel Wertheim <daniel@wertheim.se>
2012-02-28 20:16:34 +0100

    Corrected NuGet packing for cache providers.

--- Deploy/pack.bat
+++ Deploy/pack.bat
@@ -2,4 +2,6 @@ nuget pack sisodb.nuspec
 nuget pack sisodb.sql2008.nuspec
 nuget pack sisodb.sql2012.nuspec
 nuget pack sisodb.sqlce4.nuspec
+nuget pack sisodb.aspwebcache.nuspec
+nuget pack sisodb.msmemorycache.nuspec
 pause
