c4e6646c3c83253c448748eef5ac37305ea252db
Daniel Wertheim <daniel@wertheim.se>
2012-03-01 11:02:12 +0100

    Casting extension

---
+++ Source/Projects/SisoDb/Core/CastExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace SisoDb.Core
+{
+    public static class CastExtensions //TODO: Move to NCore
+    {
+        public static T CastAs<T>(this object obj) where T : class
+        {
+            return obj as T;
+        }
+    }
+}
