b3bbbfc5b2271b5189ea4e90a8db7bf4ac81d593
Daniel Wertheim <daniel@wertheim.se>
2012-03-29 00:19:55 +0200

    Added SisoDb.Specifications.SqlProfiler

---
+++ Source/Tests/SisoDb.Specifications.SqlProfiler/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SisoDb.Specifications.SqlProfiler")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2311d321-aa1f-4f44-80c2-b5e74332a686")]
---
+++ Source/Tests/SisoDb.Testing/SqlProfiler/SqlProfilerTestContext.cs
@@ -0,0 +1,13 @@
+﻿using SisoDb.Sql2012;
+
+namespace SisoDb.Testing.SqlProfiler
+{
+    public class SqlProfilerTestContext : TestContextBase
+    {
+        public SqlProfilerTestContext(string connectionStringName)
+            : base(new Sql2012DbFactory(), new Sql2012ConnectionInfo(connectionStringName), new Sql2012ProviderFactory())
+        {
+            DbHelper = new SqlProfilerTestDbUtils(Database.ConnectionInfo.ClientConnectionString.PlainString);
+        }
+    }
+}
