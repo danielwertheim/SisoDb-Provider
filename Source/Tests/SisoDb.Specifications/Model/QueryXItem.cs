3ee20e7548ec567f1aedfcca7f1035f7172c27aa
Daniel Wertheim <daniel@wertheim.se>
2012-03-29 11:43:09 +0200

    Verified sorting on nullable value type in queries.

--- Source/Tests/SisoDb.Specifications/Model/QueryXItem.cs
+++ Source/Tests/SisoDb.Specifications/Model/QueryXItem.cs
@@ -14,7 +14,7 @@ namespace SisoDb.Specifications.Model
             for (var c = 0; c < numOfItems; c++)
             {
                 var item = new TItem();
-                
+
                 initializer.Invoke(c, item);
 
                 items.Add(item);
@@ -27,10 +27,10 @@ namespace SisoDb.Specifications.Model
         {
             return IfStringIdApplyStringIdValue(new[]
             {
-                new TItem{SortOrder = 1, IntegerValue = 100, StringValue = "A"},
-                new TItem{SortOrder = 2, IntegerValue = 200, StringValue = "B"},
-                new TItem{SortOrder = 3, IntegerValue = 300, StringValue = "C"},
-                new TItem{SortOrder = 4, IntegerValue = 400, StringValue = "D"}
+                new TItem{SortOrder = 1, IntegerValue = 100, NullableIntegerValue = 100, StringValue = "A"},
+                new TItem{SortOrder = 2, IntegerValue = 200, NullableIntegerValue = 200, StringValue = "B"},
+                new TItem{SortOrder = 3, IntegerValue = 300, NullableIntegerValue = 300, StringValue = "C"},
+                new TItem{SortOrder = 4, IntegerValue = 400, NullableIntegerValue = 400, StringValue = "D"}
             });
         }
 
@@ -38,16 +38,16 @@ namespace SisoDb.Specifications.Model
         {
             return IfStringIdApplyStringIdValue(new[]
             {
-                new TItem{SortOrder = 1, IntegerValue = 100, StringValue = "A"},
-                new TItem{SortOrder = 2, IntegerValue = 200, StringValue = "B"},
-                new TItem{SortOrder = 3, IntegerValue = 300, StringValue = "C"},
-                new TItem{SortOrder = 4, IntegerValue = 400, StringValue = "D"},
-                new TItem{SortOrder = 5, IntegerValue = 500, StringValue = "E"},
-                new TItem{SortOrder = 6, IntegerValue = 600, StringValue = "F"},
-                new TItem{SortOrder = 7, IntegerValue = 700, StringValue = "G"},
-                new TItem{SortOrder = 8, IntegerValue = 800, StringValue = "H"},
-                new TItem{SortOrder = 9, IntegerValue = 900, StringValue = "I"},
-                new TItem{SortOrder = 10, IntegerValue = 1000, StringValue = "J"}
+                new TItem{SortOrder = 1, IntegerValue = 100, NullableIntegerValue = 100, StringValue = "A"},
+                new TItem{SortOrder = 2, IntegerValue = 200, NullableIntegerValue = 200, StringValue = "B"},
+                new TItem{SortOrder = 3, IntegerValue = 300, NullableIntegerValue = 300, StringValue = "C"},
+                new TItem{SortOrder = 4, IntegerValue = 400, NullableIntegerValue = 400, StringValue = "D"},
+                new TItem{SortOrder = 5, IntegerValue = 500, NullableIntegerValue = 500, StringValue = "E"},
+                new TItem{SortOrder = 6, IntegerValue = 600, NullableIntegerValue = 600, StringValue = "F"},
+                new TItem{SortOrder = 7, IntegerValue = 700, NullableIntegerValue = 700, StringValue = "G"},
+                new TItem{SortOrder = 8, IntegerValue = 800, NullableIntegerValue = 800, StringValue = "H"},
+                new TItem{SortOrder = 9, IntegerValue = 900, NullableIntegerValue = 900, StringValue = "I"},
+                new TItem{SortOrder = 10, IntegerValue = 1000, NullableIntegerValue = 1000, StringValue = "J"}
             });
         }
 
@@ -55,10 +55,10 @@ namespace SisoDb.Specifications.Model
         {
             return IfStringIdApplyStringIdValue(new[]
             {
-                new TItem{SortOrder = 2, IntegerValue = 400, StringValue = "D"},
-                new TItem{SortOrder = 2, IntegerValue = 300, StringValue = "C"},
-                new TItem{SortOrder = 1, IntegerValue = 200, StringValue = "B"},
-                new TItem{SortOrder = 1, IntegerValue = 100, StringValue = "A"}
+                new TItem{SortOrder = 2, IntegerValue = 400, NullableIntegerValue = 400, StringValue = "D"},
+                new TItem{SortOrder = 2, IntegerValue = 300, NullableIntegerValue = 300, StringValue = "C"},
+                new TItem{SortOrder = 1, IntegerValue = 200, NullableIntegerValue = 200, StringValue = "B"},
+                new TItem{SortOrder = 1, IntegerValue = 100, NullableIntegerValue = 100, StringValue = "A"}
             });
         }
 
@@ -78,6 +78,8 @@ namespace SisoDb.Specifications.Model
         public int SortOrder { get; set; }
 
         public int IntegerValue { get; set; }
+
+        public int? NullableIntegerValue { get; set; }
 
         public string StringValue { get; set; }
 
