3ea44fe4fcdae0e27ebbb8d9292fded3019148e0
Daniel Wertheim <daniel@wertheim.se>
2012-04-04 23:15:50 +0200

    Bumped to 11.2.0

--- Deploy/pack.bat
+++ Deploy/pack.bat
@@ -1,8 +1,9 @@
-nuget pack sisodb.nuspec -version 11.1.0
-nuget pack sisodb.sql2008.nuspec -version 11.1.0
-nuget pack sisodb.sql2012.nuspec -version 11.1.0
-nuget pack sisodb.sqlce4.nuspec -version 11.1.0
-nuget pack sisodb.miniprofiler.nuspec -version 11.1.0
-nuget pack sisodb.aspwebcache.nuspec -version 11.1.0
-nuget pack sisodb.msmemorycache.nuspec -version 11.1.0
+nuget pack sisodb.nuspec -version 11.2.0
+nuget pack sisodb.dynamic.nuspec -version 11.2.0
+nuget pack sisodb.sql2008.nuspec -version 11.2.0
+nuget pack sisodb.sql2012.nuspec -version 11.2.0
+nuget pack sisodb.sqlce4.nuspec -version 11.2.0
+nuget pack sisodb.miniprofiler.nuspec -version 11.2.0
+nuget pack sisodb.aspwebcache.nuspec -version 11.2.0
+nuget pack sisodb.msmemorycache.nuspec -version 11.2.0
 pause
