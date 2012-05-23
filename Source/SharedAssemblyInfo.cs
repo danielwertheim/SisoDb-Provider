dd4f97bd622a5580e5a6123cfe6acd48def2364f
Daniel Wertheim <daniel@wertheim.se>
2012-05-23 08:11:24 +0200

    For pre-release.

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -13,5 +13,5 @@
 [assembly: AssemblyCopyright("Copyright © Daniel Wertheim")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("12.0.0")]
-[assembly: AssemblyFileVersion("12.0.0")]
+[assembly: AssemblyVersion("13.0.0")]
+[assembly: AssemblyFileVersion("13.0.0")]
