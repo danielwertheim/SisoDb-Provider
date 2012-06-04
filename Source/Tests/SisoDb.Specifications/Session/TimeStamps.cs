599aa27c6f37b8ff275a0ed10f1c501c3f6459b0
Daniel Wertheim <daniel@wertheim.se>
2012-06-04 22:21:39 +0200

    Fixed timestamp tests after isolating dates.

--- Source/Tests/SisoDb.Specifications/Session/TimeStamps.cs
+++ Source/Tests/SisoDb.Specifications/Session/TimeStamps.cs
@@ -14,8 +14,12 @@ namespace SisoDb.Specifications.Session
             Establish context = () =>
             {
                 TestContext = TestContextFactory.Create();
-                _beforeExecution = SysDateTime.Now;
+
                 _orgItem = new Model { StringValue = "Org string" };
+
+                _beforeExecution = SysDateTime.Now;
+                _fixedDateTime = _beforeExecution.AddSeconds(1);
+                SysDateTime.NowFn = () => _fixedDateTime;
             };
 
             Because of = () => TestContext.Database.UseOnceTo().Insert(_orgItem);
@@ -23,10 +27,14 @@ namespace SisoDb.Specifications.Session
             It should_only_have_one_item_stored =
                 () => TestContext.Database.should_have_X_num_of_items<Model>(1);
 
-            It should_have_assigned_value_to_timestamp =
-                () => _orgItem.TimeStamp.ShouldBeGreaterThan(_beforeExecution);
+            It should_have_assigned_value_to_timestamp = () =>
+            {
+                _orgItem.TimeStamp.ShouldBeGreaterThan(_beforeExecution);
+                _orgItem.TimeStamp.ShouldEqual(_fixedDateTime);
+            };
 
             private static DateTime _beforeExecution;
+            private static DateTime _fixedDateTime;
             private static Model _orgItem;
         }
 
@@ -36,9 +44,13 @@ namespace SisoDb.Specifications.Session
             Establish context = () =>
             {
                 TestContext = TestContextFactory.Create();
+
                 _orgItem = new Model { StringValue = "Org string" };
                 TestContext.Database.UseOnceTo().Insert(_orgItem);
+
                 _beforeExecution = _orgItem.TimeStamp;
+                _fixedDateTime = _beforeExecution.AddSeconds(1);
+                SysDateTime.NowFn = () => _fixedDateTime;
             };
 
             Because of = () => TestContext.Database.UseOnceTo().Update(_orgItem);
@@ -46,10 +58,14 @@ namespace SisoDb.Specifications.Session
             It should_only_have_one_item_stored =
                 () => TestContext.Database.should_have_X_num_of_items<Model>(1);
 
-            It should_have_assigned_new_value_to_timestamp =
-                () => _orgItem.TimeStamp.ShouldBeGreaterThan(_beforeExecution);
+            It should_have_assigned_new_value_to_timestamp = () =>
+            {
+                _orgItem.TimeStamp.ShouldBeGreaterThan(_beforeExecution);
+                _orgItem.TimeStamp.ShouldEqual(_fixedDateTime);
+            };
 
             private static DateTime _beforeExecution;
+            private static DateTime _fixedDateTime;
             private static Model _orgItem;
         }
 
@@ -59,9 +75,13 @@ namespace SisoDb.Specifications.Session
             Establish context = () =>
             {
                 TestContext = TestContextFactory.Create();
+
                 _orgItem = new Model { StringValue = "Org string" };
                 TestContext.Database.UseOnceTo().Insert(_orgItem);
+
                 _beforeExecution = _orgItem.TimeStamp;
+                _fixedDateTime = _beforeExecution.AddSeconds(1);
+                SysDateTime.NowFn = () => _fixedDateTime;
             };
 
             Because of = () => TestContext.Database.UseOnceTo().Update<Model>(_orgItem.Id, x => x.StringValue = "Foooooo");
@@ -69,14 +89,15 @@ namespace SisoDb.Specifications.Session
             It should_only_have_one_item_stored =
                 () => TestContext.Database.should_have_X_num_of_items<Model>(1);
 
-            It should_have_assigned_new_value_to_timestamp =
-                () =>
-                {
-                    var refetched = TestContext.Database.UseOnceTo().GetById<Model>(_orgItem.Id);
-                    refetched.TimeStamp.ShouldBeGreaterThan(_beforeExecution);
-                };
+            It should_have_assigned_new_value_to_timestamp = () =>
+            {
+                var refetched = TestContext.Database.UseOnceTo().GetById<Model>(_orgItem.Id);
+                refetched.TimeStamp.ShouldBeGreaterThan(_beforeExecution);
+                refetched.TimeStamp.ShouldEqual(_fixedDateTime);
+            };
 
             private static DateTime _beforeExecution;
+            private static DateTime _fixedDateTime;
             private static Model _orgItem;
         }
 
