c2b4f2aa2e6b32b638790163f464959ca827fbf5
Daniel Wertheim <daniel@wertheim.se>
2012-07-25 21:48:38 +0200

    - Fixed AnyIndexesTableHasMember to honor the Id being passed. - Fixed UniquesTableHasMember to honor the Id being passed. - Reworked ITestDbUtils to use AdoDriver and support parameters

--- Source/Tests/SisoDb.Testing/SqlServerTestDbUtils.cs
+++ Source/Tests/SisoDb.Testing/SqlServerTestDbUtils.cs
@@ -27,26 +27,19 @@ namespace SisoDb.Testing
         {
             var sql = "select 1 from INFORMATION_SCHEMA.TABLES where TABLE_NAME = '{0}';".Inject(name);
 
-            return ExecuteNullableScalar<int>(CommandType.Text, sql).HasValue;
+            return ExecuteNullableScalar<int>(sql).HasValue;
         }
 
         public virtual bool TablesExists(string[] names)
         {
-            foreach (var name in names)
-            {
-                var exists = TableExists(name);
-                if (!exists)
-                    return false;
-            }
-
-            return true;
+            return names.Select(TableExists).All(exists => exists);
         }
 
         public virtual bool TypeExists(string name)
         {
             var sql = "select 1 from sys.table_types where name = '{0}';".Inject(name);
 
-            return ExecuteNullableScalar<int>(CommandType.Text, sql).HasValue;
+            return ExecuteNullableScalar<int>(sql).HasValue;
         }
 
         public virtual IList<DbColumn> GetColumns(string tableName, params string[] namesToSkip)
@@ -80,26 +73,23 @@ namespace SisoDb.Testing
 
         public virtual void CreateProcedure(string spSql)
         {
-            ExecuteSql(CommandType.Text, spSql);
+            ExecuteSql(spSql);
         }
 
         public virtual void DropProcedure(string spName)
         {
-            ExecuteSql(CommandType.Text, "if(select OBJECT_ID('{0}', 'P')) is not null begin drop procedure {0}; end".Inject(spName));
+            ExecuteSql("if(select OBJECT_ID('{0}', 'P')) is not null begin drop procedure {0}; end".Inject(spName));
         }
 
-        public virtual T ExecuteScalar<T>(CommandType commandType, string sql)
+        public virtual T ExecuteScalar<T>(string sql, params IDacParameter[] parameters)
         {
             T retVal;
 
             using (var cn = CreateConnection())
             {
                 cn.Open();
-                using (var cmd = cn.CreateCommand())
+                using (var cmd = Driver.CreateCommand(cn, sql, null, parameters))
                 {
-                    cmd.CommandType = commandType;
-                    cmd.CommandText = sql;
-
                     var value = cmd.ExecuteScalar();
 
                     if (value == null || value == DBNull.Value)
@@ -113,18 +103,15 @@ namespace SisoDb.Testing
             return retVal;
         }
 
-        public virtual T? ExecuteNullableScalar<T>(CommandType commandType, string sql) where T : struct
+        public virtual T? ExecuteNullableScalar<T>(string sql) where T : struct
         {
             T? retVal;
 
             using (var cn = CreateConnection())
             {
                 cn.Open();
-                using (var cmd = cn.CreateCommand())
+                using (var cmd = Driver.CreateCommand(cn, sql))
                 {
-                    cmd.CommandType = commandType;
-                    cmd.CommandText = sql;
-
                     var value = cmd.ExecuteScalar();
 
                     if (value == null || value == DBNull.Value)
@@ -138,12 +125,12 @@ namespace SisoDb.Testing
             return retVal;
         }
 
-        public virtual int RowCount(string tableName, string where = null)
+        public virtual int RowCount(string tableName, string where = null, params IDacParameter[] parameters)
         {
             where = where ?? "1 = 1";
             var sql = "select count(*) from [{0}] where {1};".Inject(tableName, where);
 
-            return ExecuteScalar<int>(CommandType.Text, sql);
+            return ExecuteScalar<int>(sql, parameters);
         }
 
         public virtual void DeleteQueryIndexesFor(IStructureSchema structureSchema, IEnumerable<Guid> structureIds)
@@ -155,10 +142,8 @@ namespace SisoDb.Testing
             using (var cn = CreateConnection())
             {
                 cn.Open();
-                using (var cmd = cn.CreateCommand())
+                using (var cmd = Driver.CreateCommand(cn, null))
                 {
-                    cmd.CommandType = CommandType.Text;
-
                     foreach (var tableName in indexesTableNames.All)
                     {
                         cmd.CommandText = sqlFormat.Inject(tableName);
@@ -169,28 +154,31 @@ namespace SisoDb.Testing
             }
         }
 
-        public virtual bool AnyIndexesTableHasMember<T>(IStructureSchema structureSchema, ValueType id, Expression<Func<T, object>> member) where T : class
+        public virtual bool AnyIndexesTableHasMember<T>(IStructureSchema structureSchema, object id, Expression<Func<T, object>> member) where T : class
         {
             var memberPath = GetMemberPath(member);
             var indexesTableNames = structureSchema.GetIndexesTableNames();
             foreach (var indexesTableName in indexesTableNames.All)
-                if (RowCount(indexesTableName, "[{0}] = '{1}'".Inject(IndexStorageSchema.Fields.MemberPath.Name, memberPath)) > 0)
+            {
+                var sql = "[{0}] = @pStructureId and [{1}] = @pPath".Inject(IndexStorageSchema.Fields.StructureId.Name, IndexStorageSchema.Fields.MemberPath.Name);
+                if (RowCount(indexesTableName, sql, new DacParameter("pStructureId", id), new DacParameter("pPath", memberPath)) > 0)
                     return true;
-
+            }
             return false;
         }
 
-        public virtual bool UniquesTableHasMember<T>(IStructureSchema structureSchema, ValueType id, Expression<Func<T, object>> member) where T : class
+        public virtual bool UniquesTableHasMember<T>(IStructureSchema structureSchema, object id, Expression<Func<T, object>> member) where T : class
         {
             var memberPath = GetMemberPath(member);
-            return RowCount(structureSchema.GetUniquesTableName(), "[{0}] = '{1}'".Inject(UniqueStorageSchema.Fields.UqMemberPath.Name, memberPath)) > 0;
+            var sql = "[{0}] = @pStructureId and [{1}] = @pPath".Inject(UniqueStorageSchema.Fields.StructureId.Name, UniqueStorageSchema.Fields.UqMemberPath.Name);
+            return RowCount(structureSchema.GetUniquesTableName(), sql, new DacParameter("pStructureId", id), new DacParameter("pPath", memberPath)) > 0;
         }
 
         public virtual bool IdentityRowExistsForSchema(IStructureSchema structureSchema)
         {
             var sql = "select 1 from SisoDbIdentities where EntityName = '{0}';".Inject(structureSchema.Name);
 
-            return ExecuteNullableScalar<int>(CommandType.Text, sql).HasValue;
+            return ExecuteNullableScalar<int>(sql).HasValue;
         }
 
         protected virtual string GetMemberPath<T>(Expression<Func<T, object>> e)
@@ -206,9 +194,6 @@ namespace SisoDb.Testing
 
                 using (var cmd = Driver.CreateCommand(cn, sql, null, parameters))
                 {
-                    cmd.CommandType = CommandType.Text;
-                    cmd.CommandText = sql;
-
                     using (var reader = cmd.ExecuteReader(CommandBehavior.SingleResult | CommandBehavior.SequentialAccess))
                     {
                         while (reader.Read())
@@ -223,16 +208,13 @@ namespace SisoDb.Testing
             }
         }
 
-        protected virtual void ExecuteSql(CommandType commandType, string sql)
+        protected virtual void ExecuteSql(string sql)
         {
             using (var cn = CreateConnection())
             {
                 cn.Open();
-                using (var cmd = cn.CreateCommand())
+                using (var cmd = Driver.CreateCommand(cn, sql))
                 {
-                    cmd.CommandType = commandType;
-                    cmd.CommandText = sql;
-
                     cmd.ExecuteNonQuery();
                 }
                 cn.Close();
