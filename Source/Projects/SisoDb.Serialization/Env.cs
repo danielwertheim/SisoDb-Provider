aa3c47f46827bad046fcc19c2f5528fd1ccaa876
Daniel Wertheim <daniel@wertheim.se>
2012-07-30 12:23:33 +0200

    Updated to latest version of ServiceStack.Text to get contributed bug fix with UTC dates when using Iso8601.

--- Source/Projects/SisoDb.Serialization/Env.cs
+++ Source/Projects/SisoDb.Serialization/Env.cs
@@ -23,7 +23,7 @@ namespace SisoDb.Serialization
 				+ (IsMonoTouch ? " MonoTouch" : "");
 		}
 
-		public static decimal ServiceStackVersion = 3.85m;
+		public static decimal ServiceStackVersion = 3.93m;
 
 		public static bool IsUnix { get; set; }
 
