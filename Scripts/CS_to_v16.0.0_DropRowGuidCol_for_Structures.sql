cd84dd3e46a32a5457fb7fbc687d97588bdaef59
Daniel Wertheim <daniel@wertheim.se>
2012-11-27 21:21:17 +0100

    Added some change scripts

---
+++ Scripts/CS_to_v16.0.0_DropRowGuidCol_for_Structures.sql
@@ -0,0 +1,31 @@
+/************************************************/
+-- 2012-11-27, v1
+-- No warranties. Use at own risk
+-- ENSURE YOU HAVE A BACKUP OF YOUR DB
+-- Used to drop rowguidcol from structure tables. This is a change introduced in v16.0.0
+/************************************************/
+--use [your_db_here]
+go
+declare @t table(r int identity, n nvarchar(max))
+declare @s nvarchar(max)
+declare @n int;
+
+insert into @t
+select 'alter table dbo.[' + t.name + '] alter column [' + c.name + '] drop rowguidcol;'
+from sys.columns c 
+inner join sys.tables t on c.object_id = t.object_id 
+where c.name = 'StructureId' and c.is_rowguidcol = 1
+
+set @n = 1;
+
+print 'begin tran'
+print 'go'
+while @n <= (select max(r) from @t)
+begin
+	set @s = (select n from @t where r = @n)
+	print @s
+	set @n +=1;
+end
+print 'go'
+print '--commit tran'
+print '--rollback tran'
---
+++ Scripts/CS_to_v16.1.0_NewIndexes.sql
@@ -0,0 +1,49 @@
+/************************************************/
+-- 2012-11-27, v1
+-- No warranties. Use at own risk
+-- ENSURE YOU HAVE A BACKUP OF YOUR DB
+-- Used to generate SQL for applying new v16.1.0 indices to Indexes-tables
+/************************************************/
+--use [your_db_here]
+go
+declare @t table(r int identity, n nvarchar(max))
+declare @s nvarchar(max)
+declare @n int;
+
+insert into @t
+select 'drop index [' + i.name + '] on dbo.[' + t.name + '];'
+from sys.indexes i
+inner join sys.tables t on i.object_id = t.object_id
+where i.name like 'IX_%Strings_Q' or i.name like 'IX_%Texts_Q';
+
+insert into @t
+select 'create index [' + i.name + '] on dbo.[' + t.name + ']([MemberPath] asc, [Value] asc);'
+from sys.indexes i
+inner join sys.tables t on i.object_id = t.object_id
+where i.name like 'IX_%Strings_Q';
+
+insert into @t
+select 'create index [' + i.name + '] on dbo.[' + t.name + ']([MemberPath] asc) include([Value]);'
+from sys.indexes i
+inner join sys.tables t on i.object_id = t.object_id
+where i.name like 'IX_%Texts_Q';
+
+insert into @t
+select 'create index [IX_' + t.name + '_SID] on dbo.[' + t.name + ']([StructureId] asc);'
+from sys.indexes i
+inner join sys.tables t on i.object_id = t.object_id
+where i.name like 'IX_%Strings_Q' or i.name like 'IX_%Texts_Q';
+
+set @n = 1;
+
+print 'begin tran'
+print 'go'
+while @n <= (select max(r) from @t)
+begin
+	set @s = (select n from @t where r = @n)
+	print @s
+	set @n +=1;
+end
+print 'go'
+print '--commit tran'
+print '--rollback tran'
